67de85317831a45907d321cda3349978fdac3626
sergioitremotejobs2025-sketch <sergioitremotejobs2025@gmail.com>
2026-02-09 10:55:09 +0100

    initial.py

--- recommendations/migrations/0001_initial.py
+++ recommendations/migrations/0001_initial.py
@@ -15,6 +15,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL("CREATE EXTENSION IF NOT EXISTS vector;", reverse_sql="DROP EXTENSION IF EXISTS vector;"),
         migrations.CreateModel(
             name='Book',
             fields=[
