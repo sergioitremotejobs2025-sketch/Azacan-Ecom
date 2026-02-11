693705e6381e613925c3a3243f0417becfd89f7d
sergioitremotejobs2025-sketch <sergioitremotejobs2025@gmail.com>
2026-02-11 16:21:07 +0100

    refactor: Update `payment/urls.py` to directly import view functions instead of the `views` module.

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import payment_success, checkout, billing_info, process_order
 
 urlpatterns = [
-    path('payment_success', views.payment_success, name='payment_success'),
-    path('checkout', views.checkout, name='checkout'),
-    path('billing_info', views.billing_info, name='billing_info'),
-    path('process_order', views.process_order, name='process_order'),
+    path('payment_success', payment_success, name='payment_success'),
+    path('checkout', checkout, name='checkout'),
+    path('billing_info', billing_info, name='billing_info'),
+    path('process_order', process_order, name='process_order'),
 ]
