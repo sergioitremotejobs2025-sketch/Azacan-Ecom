069c0d28cd2ac9c3575d0a7fd55b791d35ab9921
sergioitremotejobs2025-sketch <sergioitremotejobs2025@gmail.com>
2026-02-11 10:59:29 +0100

    feat: Redesign the login page with a modern Bootstrap 5 layout and update Django settings to include `ALLOWED_HOSTS` and `DEFAULT_AUTO_FIELD`.

--- ecom/settings.py
+++ ecom/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-c7o6#9$gv)6geq@e-9*8!kj+!%bfxszcgcw^=8izgx%(!j670n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -167,3 +167,5 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = False
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
